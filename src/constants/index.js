b2abc0c88e789e4a7e22820f918f3e940cdd4bdc
Anass Lazaar <anasslazaar9@gmail.com>
2026-07-05 04:44:23 +0100

    feat: implement Safari window-where i can share my recently posted blogs or articles

--- src/constants/index.js
+++ src/constants/index.js
@@ -81,25 +81,11 @@ const dockApps = [
 const blogPosts = [
   {
     id: 1,
-    date: "Sep 2, 2025",
+    date: "7 Months ago",
     title:
-      "TypeScript Explained: What It Is, Why It Matters, and How to Master It",
-    image: "/images/blog1.png",
-    link: "https://jsmastery.com/blog/typescript-explained-what-it-is-why-it-matters-and-how-to-master-it",
-  },
-  {
-    id: 2,
-    date: "Aug 28, 2025",
-    title: "The Ultimate Guide to Mastering Three.js for 3D Development",
-    image: "/images/blog2.png",
-    link: "https://jsmastery.com/blog/the-ultimate-guide-to-mastering-three-js-for-3d-development",
-  },
-  {
-    id: 3,
-    date: "Aug 15, 2025",
-    title: "The Ultimate Guide to Mastering GSAP Animations",
-    image: "/images/blog3.png",
-    link: "https://jsmastery.com/blog/the-ultimate-guide-to-mastering-gsap-animations",
+      "NPM: The Tool Everyone Uses… But Almost No One Actually Understands",
+    image: "/images/blog-1.png",
+    link: "https://www.linkedin.com/posts/anlazaar_npm-the-tool-everyone-uses-but-almost-activity-7395240749224628224-J7TH?utm_source=share&utm_medium=member_desktop&rcm=ACoAACwjW-MBMhMDGNG4CAvqkHjtKoD2ILDjSXA",
   },
 ];
 
